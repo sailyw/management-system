af443d08fa541280fe697abb1717d54b88c99052
sailyw <1152827086@qq.com>
2020-06-05 16:16:18 +0800

    配置eslint以及其他环境配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //添加自定义规则
+    'prettier/prettier': [
+      //eslint校验不成功后，error或2则报错，warn为1则警告，off或0则无提示
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 80
+      }
+    ]
   }
 };
