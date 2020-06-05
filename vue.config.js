5a72ae5c1130a1fddde75d59125f7ccfec1ed678
sailyw <1152827086@qq.com>
2020-06-05 16:38:24 +0800

    配置scss全局

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 module.exports = {
+  // 配置项目启动端口及自动打开浏览器
   devServer: {
     port: 3333,
     open: true
+  },
+  // 配置css全局变量
+  css: {
+    loaderOptions: {
+      sass: {
+        // 新版本
+        prependData: `@import "@/assets/scss/_variable.scss";`
+        // 旧版本
+        // data: `@import "@/assets/scss/_variable.scss";`
+      }
+    }
   }
 };
