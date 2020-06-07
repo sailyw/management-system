df5c52728b6e88a8cf6e9af6b3bdc4eef587e3a0
sailyw <1152827086@qq.com>
2020-06-07 14:43:38 +0800

    表单组件封装

--- src/views/UserManage/UserManage.vue
+++ src/views/UserManage/UserManage.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    userManage
+  <div class="manage">
+    <div class="manage-header">
+      <el-button type="primary">+ 新增</el-button>
+      <common-form inline :formLabel="formLabel" :form="searchForm">
+        <el-button type="primary">搜索</el-button>
+      </common-form>
+    </div>
+    <div class="manage-content">
+      <common-table></common-table>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import CommonForm from '@/components/CommonForm';
+import CommonTable from '@/components/CommonTable';
+export default {
+  components: {
+    CommonForm,
+    CommonTable
+  },
+  data() {
+    return {
+      searchForm: {
+        keyword: ''
+      },
+      formLabel: [
+        {
+          model: 'keyword',
+          label: ''
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/common';
+</style>
