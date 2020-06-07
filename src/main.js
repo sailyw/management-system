abb75d2c59d53dac4718616c2c5ddc0b171117a9
sailyw <1152827086@qq.com>
2020-06-07 09:36:25 +0800

    axios全局配置

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-//必须在饿了么之前
+
+// 全部配置 必须在饿了么之前
 import '@/assets/scss/reset.scss';
-import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import http from '@/api/config';
+
+//第三方包
+import ElementUI from 'element-ui';
+
+Vue.use(ElementUI);
+Vue.prototype.$http = http;
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
