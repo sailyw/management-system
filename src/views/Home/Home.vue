12346691aed3cfb5c074acb5f098010997381e01
sailyw <1152827086@qq.com>
2020-06-07 10:54:29 +0800

    完成首页除图表外的内容

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <el-row class="home" :gutter="20" style="margin-left:0;margin-right:0;">
     <el-col :span="8">
-      <el-card shadow="hover" style="height:290px">
-        鼠标悬浮时显示
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImg" />
+          <div class="userinfo">
+            <p class="name">Nick</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2020-06-07</span></p>
+          <p>上次登录地点：<span>北京</span></p>
+        </div>
       </el-card>
       <el-card shadow="hover" style="height:522px;margin-top:20px;">
         鼠标悬浮时显示
@@ -10,11 +20,16 @@
     </el-col>
     <el-col :span="16">
       <div class="num">
-        <el-card shadow="hover" v-for="item in 6" :key="item">
-          <i class="icon"></i>
+        <el-card
+          shadow="hover"
+          v-for="item in countData"
+          :key="item.name"
+          :body-style="{ display: 'flex', padding: 0 }"
+        >
+          <i class="icon" :class="`el-icon-${item.icon}`" :style="{ background: item.color }"></i>
           <div class="detail">
-            <p class="num">￥1234</p>
-            <p class="txt">今日支付订单</p>
+            <p class="num">￥{{ item.value }}</p>
+            <p class="txt">{{ item.name }}</p>
           </div>
         </el-card>
       </div>
@@ -35,6 +50,49 @@
 
 <script>
 export default {
+  data() {
+    return {
+      userImg: require('../../assets/images/user.png'),
+      countData: [
+        {
+          name: '今日支付订单',
+          value: 1234,
+          icon: 'success',
+          color: '#2ec7c9'
+        },
+        {
+          name: '今日收藏订单',
+          value: 210,
+          icon: 'star-on',
+          color: '#ffb980'
+        },
+        {
+          name: '今日未支付订单',
+          value: 1234,
+          icon: 's-goods',
+          color: '#5ab1ef'
+        },
+        {
+          name: '本月支付订单',
+          value: 1234,
+          icon: 'success',
+          color: '#2ec7c9'
+        },
+        {
+          name: '本月收藏订单',
+          value: 210,
+          icon: 'star-on',
+          color: '#ffb980'
+        },
+        {
+          name: '本月未支付订单',
+          value: 1234,
+          icon: 's-goods',
+          color: '#5ab1ef'
+        }
+      ]
+    };
+  },
   mounted() {
     this.$http.get('/home/getData').then(res => {
       console.log(res.data);
